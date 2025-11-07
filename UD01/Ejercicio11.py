221c24a3dc6371aa3a085d32d42ba484751e7114
juanr <juanrfjerez@gmail.com>
2025-11-07 19:21:58 +0100

    Ej11

---
+++ UD01/Ejercicio11.py
@@ -0,0 +1,10 @@
+# Ejercicio11: Dibuja un ordinograma de un programa que muestre los números pares comprendidos
+# entre el 1 y el 200. Para ello utiliza un contador y suma de 2 en 2
+
+# Inicializamos el contador en 2, que es el primer número par mayor que 1
+contador = 2
+
+# Mientras el contador sea menor o igual a 200
+while contador <= 200:
+    print(contador)
+    contador += 2  # Sumamos de 2 en 2 para obtener solo pares
