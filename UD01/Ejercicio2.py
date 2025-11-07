54d07ff2ec5053cb46faa50bcfa214fae9a9d313
juanr <juanrfjerez@gmail.com>
2025-11-07 19:12:00 +0100

    Ej2

--- UD01/Ejercicio2.py
+++ UD01/Ejercicio2.py
@@ -1,6 +1,6 @@
 # Ejercicio2: Dibuja un ordinograma que calcule y muestre el área de un cuadrado de lado igual a 5 
 # (El área del cuadrado es igual a lado por lado).
-#hola
+
 lado = 5
 area = lado * lado
 
