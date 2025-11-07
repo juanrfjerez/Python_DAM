936a465fe26599b0417167044940f25b6c4c2bec
juanr <juanrfjerez@gmail.com>
2025-11-07 19:12:37 +0100

    Ej3

--- UD01/Ejercicio3.py
+++ UD01/Ejercicio3.py
@@ -1,6 +1,6 @@
 #Ejercicio3: Dibuja un ordinograma que calcule el área de un cuadrado cuyo lado 
 # se introduce por teclado (El área del cuadrado es igual a lado por lado).
-#ho
+
 lado = float(input("Introduce el valor del lado del cuadrado: "))
 
 area = lado * lado
