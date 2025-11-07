cbbdb9d65bda564dd0c8700416ad556fea208cba
juanr <juanrfjerez@gmail.com>
2025-11-07 19:15:00 +0100

    Ej10

---
+++ UD01/Ejercicio10.py
@@ -0,0 +1,6 @@
+# Ejercicio10: Dibuja un ordinograma de un programa que muestre por pantalla
+# los 20 primeros números naturales (1, 2, 3 …. 20)
+
+# Muestra los números del 1 al 20
+for numero in range(1, 21):
+    print(numero)
