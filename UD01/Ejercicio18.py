db9f75a0760e74942e22675753bb94290b040953
juanr <juanrfjerez@gmail.com>
2025-11-07 20:06:51 +0100

    Ej18

---
+++ UD01/Ejercicio18.py
@@ -0,0 +1,14 @@
+# Ejercicio18: Dibuja un ordinograma de un programa que lea dos números
+# y nos diga cual es mayor o si son iguales
+
+# Solicita dos números al usuario
+num1 = float(input("Introduce el primer número: "))
+num2 = float(input("Introduce el segundo número: "))
+
+# Compara los dos números y muestra el resultado
+if num1 > num2:
+    print(f"El mayor es: {num1}")
+elif num2 > num1:
+    print(f"El mayor es: {num2}")
+else:
+    print("Ambos números son iguales")
